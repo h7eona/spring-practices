8cea5c7c3b541b5d95e9b6f4d7292d93fac90874
h7eona <jessie117@naver.com>
2024-12-27 08:46:04 +0900

    guestbook03 제출

---
+++ guestbook03/src/main/java/guestbook/controller/GuestbookController.java
@@ -0,0 +1,45 @@
+package guestbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import guestbook.repository.GuestbookRepository;
+import guestbook.vo.GuestbookVo;
+
+@Controller
+public class GuestbookController {
+	private GuestbookRepository guestbookRepository;
+	
+	public GuestbookController(GuestbookRepository guestbookRepository) {
+		this.guestbookRepository = guestbookRepository;
+	}
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("list", guestbookRepository.findAll());
+		return "index";
+	}
+	
+	@RequestMapping("/add")
+	public String add(GuestbookVo vo) {
+		guestbookRepository.insert(vo);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+	public String delete(@PathVariable("id") Long id) {
+		return "delete";
+	}
+	
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.POST)
+	public String delete(
+		@PathVariable("id") Long id,
+		@RequestParam(value="password", required=true, defaultValue="") String password) {
+		guestbookRepository.deleteByIdAndPassword(id, password);
+		return "redirect:/";
+	}
+}
