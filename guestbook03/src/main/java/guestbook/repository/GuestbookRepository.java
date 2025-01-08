4be077de0f3ecdaa63bc3d7c0e2a29e61091f88d
h7eona <jessie117@naver.com>
2025-01-08 10:59:13 +0900

    jdbcContext update

--- guestbook03/src/main/java/guestbook/repository/GuestbookRepository.java
+++ guestbook03/src/main/java/guestbook/repository/GuestbookRepository.java
@@ -1,10 +1,8 @@
 package guestbook.repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
 
 import guestbook.repository.template.JdbcContext;
@@ -18,33 +16,28 @@ public class GuestbookRepository {
 		this.jdbcContext = jdbcContext;
 	}
 	
+	public GuestbookVo findById(Long id) {
+		return jdbcContext.queryForObject(
+			"select id, name, contents, date_format(reg_date, '%Y-%m-%d') as regDate from guestbook where id = ?", 
+			new Object[] {id},
+			new BeanPropertyRowMapper<>(GuestbookVo.class));
+	}	
+	
 	public List<GuestbookVo> findAll() {
-		return jdbcContext.queryForList(
-		"select id, name, contents, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc",
-		new RowMapper<GuestbookVo>() {
-			@Override
-			public GuestbookVo mapRow(ResultSet rs, int rowNum) throws SQLException {
-				GuestbookVo vo = new GuestbookVo();
-				
-				vo.setId(rs.getLong(1));
-				vo.setName(rs.getString(2));
-				vo.setContents(rs.getString(3));
-				vo.setRegDate(rs.getString(4));
-				
-				return vo;
-			}	
-		});
+		return jdbcContext.query(
+		"select id, name, contents, date_format(reg_date, '%Y-%m-%d %h:%i:%s') as regDate from guestbook order by reg_date desc",
+		new BeanPropertyRowMapper<>(GuestbookVo.class));
 	}
 
 	public int insert(GuestbookVo vo) {
-		return jdbcContext.excuteUpdate(
+		return jdbcContext.update(
 				"insert into guestbook values(null, ?, ?, ?, now())",
-				new Object[] {vo.getName(), vo.getPassword(), vo.getContents()});
+				vo.getName(), vo.getPassword(), vo.getContents());
 	}
 
 	public int deleteByIdAndPassword(Long id, String password) {
-		return jdbcContext.excuteUpdate(
+		return jdbcContext.update(
 				"delete from guestbook where id=? and password=?",
-				new Object[] {id, password});
-	}	
+				id, password);
+	}
 }
--- guestbook03/src/main/java/guestbook/repository/template/JdbcContext.java
+++ guestbook03/src/main/java/guestbook/repository/template/JdbcContext.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
-
 import org.springframework.jdbc.core.RowMapper;
+
 
 public class JdbcContext {
 	private DataSource dataSource;
@@ -19,17 +19,32 @@ public class JdbcContext {
 		this.dataSource = dataSource;
 	}
 	
-	public <E> List<E> queryForList(String sql, RowMapper<E> rowMapper) {
-		return queryForListWithStatementStrategy(new StatementStrategy() {
+	public <E> List<E> query(String sql, RowMapper<E> rowMapper) {
+		return queryWithStatementStrategy(new StatementStrategy() {
 			@Override
 			public PreparedStatement makeStatement(Connection connection) throws SQLException {
 				return connection.prepareStatement(sql);
 			}
 		}, rowMapper);
 	}
+	
+	public <E> E queryForObject(String sql, Object[] parameters, RowMapper<E> rowMapper) {
+		return queryForObjectWithStatementStrategy(new StatementStrategy() {
+			@Override
+			public PreparedStatement makeStatement(Connection connection) throws SQLException {
+				PreparedStatement pstmt = connection.prepareStatement(sql);
+				
+				for(int i = 0; i < parameters.length; i++) {
+					pstmt.setObject(i+1, parameters[i]);
+				}
+				
+				return pstmt;
+			}
+		}, rowMapper);
+	}
 
-	public int excuteUpdate(String sql, Object[] parameters) {
-		return executeUpdateWithStatementStrategy(new StatementStrategy() {
+	public int update(String sql, Object... parameters) {
+		return updateWithStatementStrategy(new StatementStrategy() {
 			@Override
 			public PreparedStatement makeStatement(Connection connection) throws SQLException {
 				PreparedStatement pstmt = connection.prepareStatement(sql);
@@ -43,7 +58,7 @@ public class JdbcContext {
 		});
 	}
 	
-	private <E> List<E> queryForListWithStatementStrategy(StatementStrategy statementStrategy, RowMapper<E> rowMapper) throws RuntimeException {
+	private <E> List<E> queryWithStatementStrategy(StatementStrategy statementStrategy, RowMapper<E> rowMapper) throws RuntimeException {
 		List<E> result = new ArrayList<>();
 		
 		try (
@@ -62,7 +77,23 @@ public class JdbcContext {
 		return result;
 	}
 	
-	private int executeUpdateWithStatementStrategy(StatementStrategy statementStrategy)throws RuntimeException {		
+	private <E> E queryForObjectWithStatementStrategy(StatementStrategy statementStrategy, RowMapper<E> rowMapper) {
+		try (
+			Connection conn = dataSource.getConnection();
+			PreparedStatement pstmt = statementStrategy.makeStatement(conn);
+			ResultSet rs = pstmt.executeQuery();
+		) {
+			if(rs.next()) {
+				return rowMapper.mapRow(rs,  rs.getRow());
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+			
+		return null;
+	}
+	
+	private int updateWithStatementStrategy(StatementStrategy statementStrategy)throws RuntimeException {		
 		int count = 0;
 		
 		try (
@@ -76,4 +107,5 @@ public class JdbcContext {
 		
 		return count;	
 	}
+
 }
